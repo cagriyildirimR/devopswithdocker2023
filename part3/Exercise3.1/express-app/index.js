5135cd9001a42ac7ddf88751ffb454b8be64d437
Cagri Yildirim <cagri.yildirim.r@gmail.com>
2023-06-05 16:26:48 +0300

    Improve index.js

--- part3/Exercise3.1/express-app/index.js
+++ part3/Exercise3.1/express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae placerat neque. Ut viverra tincidunt mi, a lobortis odio tincidunt non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.</p>')
+  res.send('<h1>Hello World!</h1><br><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae placerat neque. Ut viverra tincidunt mi, a lobortis odio tincidunt non. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue.</p> <br> <p>This is the last paragprah</p>')
 })
 
 const PORT = 8080
